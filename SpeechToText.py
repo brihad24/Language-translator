57a09610f24185c76f30dbc63189cd2e312f774d
brihad24 <brihad1692@gmail.com>
2022-09-02 23:52:41 +0530

    Update to previous code

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,22 +1,21 @@
 import speech_recognition
 import pyttsx3
-
-recogniser = speech_recognition.Recognizer()
-
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
-            recogniser.adjust_for_ambient_noise(mic, duration = 0.2)
-            audio = recogniser.listen(mic)
-
-            text = recogniser.recognize_google(audio)
-            text = text.lower()
-
-            print(f"Recognised {text}")
-    
-    except speech_recognition.UnknownValueError():
-        recogniser = speech_recognition.Recognizer()
-        continue
         
 def speechConversion():
-    return True
+
+    recogniser = speech_recognition.Recognizer()
+
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recogniser.adjust_for_ambient_noise(mic, duration = 0.2)
+                audio = recogniser.listen(mic, phrase_time_limit= 5)
+
+                text = recogniser.recognize_google(audio)
+                text = text.lower()
+
+                return text
+        
+        except speech_recognition.UnknownValueError():
+            recogniser = speech_recognition.Recognizer()
+            continue
