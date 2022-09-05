03d21c5c900073f628188b0bad16d5ea173feaa1
brihad24 <brihad1692@gmail.com>
2022-09-05 23:03:38 +0530

    Some more updates

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,21 +1,101 @@
-import speech_recognition
-import pyttsx3
+from playsound import playsound
+import speech_recognition as sr
+from googletrans import Translator
+from gtts import gTTS
+import os
+from languages import dic
+
+# Capture Voice
+# takes command through microphone
+def takecommand():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("listening.....")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+
+	try:
+		print("Recognizing.....")
+		query = r.recognize_google(audio, language='en-in')
+		print(f"user said {query}\n")
+	except Exception as e:
+		print("say that again please.....")
+		return "None"
+	return query
+
+# Taking voice input from the user
+query = takecommand()
+while (query == "None"):
+	query = takecommand()
+
+def destination_language():
+	print("Enter the language in which you want to convert \
+	: Ex. Hindi , English , etc.")
+	print()
+
+	# Input destination language in which the user
+	# wants to translate
+	to_lang = takecommand()
+	while (to_lang == "None"):
+		to_lang = takecommand()
+	to_lang = to_lang.lower()
+	return to_lang
+
+to_lang = destination_language()
+
+# Mapping it with the code
+while (to_lang not in dic):
+	print("Language in which you are trying to convert\
+	is currently not available ,please input some other language")
+	print()
+	to_lang = destination_language()
+
+to_lang = dic[dic.index(to_lang)+1]
+
+# invoking Translator
+translator = Translator()
+
+# Translating from src to dest
+text_to_translate = translator.translate(query, dest=to_lang)
+text = text_to_translate.text
+
+# Using Google-Text-to-Speech ie, gTTS() method
+# to speak the translated text into the
+# destination language which is stored in to_lang.
+# Also, we have given 3rd argument as False because
+# by default it speaks very slowly
+speak = gTTS(text=text, lang=to_lang, slow=False)
+
+# Using save() method to save the translated
+# speech in capture_voice.mp3
+speak.save("captured_voice.mp3")
+
+# Using OS module to run the translated voice.
+playsound('captured_voice.mp3')
+os.remove('captured_voice.mp3')
+print(text)
+
+
+
+
+# import speech_recognition
+# import pyttsx3
         
-def speechConversion():
+# def speechConversion():
 
-    recogniser = speech_recognition.Recognizer()
+#     recogniser = speech_recognition.Recognizer()
 
-    while True:
-        try:
-            with speech_recognition.Microphone() as mic:
-                recogniser.adjust_for_ambient_noise(mic, duration = 0.2)
-                audio = recogniser.listen(mic, phrase_time_limit= 5)
+#     while True:
+#         try:
+#             with speech_recognition.Microphone() as mic:
+#                 recogniser.adjust_for_ambient_noise(mic, duration = 0.2)
+#                 audio = recogniser.listen(mic, phrase_time_limit= 2)
 
-                text = recogniser.recognize_google(audio)
-                text = text.lower()
+#                 text = recogniser.recognize_google(audio)
+#                 text = text.lower()
 
-                return text
+#                 return text
         
-        except speech_recognition.UnknownValueError():
-            recogniser = speech_recognition.Recognizer()
-            continue
+#         except speech_recognition.UnknownValueError():
+#             recogniser = speech_recognition.Recognizer()
+#             continue
--- mainFile.py
+++ mainFile.py
@@ -3,7 +3,8 @@ from tkinter import *
 from trace import Trace
 import tkinter.messagebox
 from turtle import bgcolor
-from SpeechToText import *
+from playsound import playsound
+import speech_recognition as sr
 
 root = Tk()
 
@@ -11,18 +12,40 @@ root.title("Language Translator")
 root.geometry("600x400+10+20")
 root.config(bg = '#485063')
 
+TextBoxText = StringVar()
+
 def buttonClick():
-    tkinter.messagebox.showinfo('Translator', 'Listening to audio...           ')
-    print(speechConversion())
+    r = sr.Recognizer()
+    global TextBoxText
+    # TranslatedText.config(state= 'normal')
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        TextBoxText.set("Listening to audio...")
+
+    try:
+        print("Recognizing.....")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said {query}\n")
+        TextBoxText.set(query)
+
+    except Exception as e:
+        print("say that again please.....")
+        TextBoxText.set("Could not catch that :(" + '\n' + "Press the button to speak again")
+    # tkinter.messagebox.showinfo('Translator', 'Listening to audio...           ')
 
 ListenButton = Button(root, text="Start listening?", fg='white', bg = '#56717d', font= ('Helvetica', 11), command= buttonClick)
 ListenButton.pack(pady = 30)
 
 Label(root, text="Translated Text:", bg= '#5d6475', fg = 'white', font = ('Times', 13)).pack(pady = 30)
 
-TranslatedText = Text(root, height= 10, width= 40)
-TranslatedText.insert('end', "Waiting for audio...")
-TranslatedText.pack()
-TranslatedText.config(state= 'disabled')
+DisplayText = Label(root, text=TextBoxText, bg= 'white', fg = 'black', font = ('Times', 13), width= 40, height= 10).pack(pady = 30)
+
+TextBoxText.set("Waiting for audio...")
+# TranslatedText = Text(root, height= 10, width= 40)
+# TranslatedText.insert('end', TextBoxText)
+# TranslatedText.pack()
+# TranslatedText.config(state= 'disabled')
 
 root.mainloop()
