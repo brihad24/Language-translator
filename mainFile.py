b22cbba65311475bbb4b6a7e11337b6ffa537fe4
brihad24 <brihad1692@gmail.com>
2022-09-08 12:13:19 +0530

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -12,40 +12,41 @@ root.title("Language Translator")
 root.geometry("600x400+10+20")
 root.config(bg = '#485063')
 
-TextBoxText = StringVar()
+global TextBoxText 
+TextBoxText = "Waiting for audio..."
 
 def buttonClick():
     r = sr.Recognizer()
     global TextBoxText
-    # TranslatedText.config(state= 'normal')
     with sr.Microphone() as source:
-        print("listening.....")
-        r.pause_threshold = 1
+        # print("listening.....")
+        r.pause_threshold = 0.5
         audio = r.listen(source)
-        TextBoxText.set("Listening to audio...")
+        TextBoxText = "Listening to audio..."
+        # TranslatedText.insert('end', "Listening to audio...")
 
     try:
-        print("Recognizing.....")
+        # print("Recognizing.....")
         query = r.recognize_google(audio, language='en-in')
         print(f"user said {query}\n")
-        TextBoxText.set(query)
+        TextBoxText = query
+        TranslatedText.delete("1.0","end")
+        TranslatedText.insert('end', TextBoxText)
 
     except Exception as e:
-        print("say that again please.....")
-        TextBoxText.set("Could not catch that :(" + '\n' + "Press the button to speak again")
-    # tkinter.messagebox.showinfo('Translator', 'Listening to audio...           ')
+        # print("say that again please.....")
+        TranslatedText.delete("1.0","end")
+        TextBoxText = "Could not catch that :(" + '\n' + "Press the button to speak again"
+        TranslatedText.insert('end', TextBoxText)
 
 ListenButton = Button(root, text="Start listening?", fg='white', bg = '#56717d', font= ('Helvetica', 11), command= buttonClick)
 ListenButton.pack(pady = 30)
 
 Label(root, text="Translated Text:", bg= '#5d6475', fg = 'white', font = ('Times', 13)).pack(pady = 30)
 
-DisplayText = Label(root, text=TextBoxText, bg= 'white', fg = 'black', font = ('Times', 13), width= 40, height= 10).pack(pady = 30)
-
-TextBoxText.set("Waiting for audio...")
-# TranslatedText = Text(root, height= 10, width= 40)
-# TranslatedText.insert('end', TextBoxText)
-# TranslatedText.pack()
+TranslatedText = Text(root, height= 10, width= 40, font=('Arial', 10))
+TranslatedText.insert('end', TextBoxText)
+TranslatedText.pack()
 # TranslatedText.config(state= 'disabled')
 
 root.mainloop()
